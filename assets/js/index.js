263e37be76e35a3b5fc5282ccff60e7aca003e40
Devin Blais <devinblais@gmail.com>
2014-04-27 16:54:11 -0700

    initializing nprogress and pjax

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,26 +4,76 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+  "use strict";
 
-    $(document).ready(function(){
+  $(function(){
 
-        $(".post-content").fitVids();
-        
-        // Calculates Reading Time
-        $('.post-content').readingTime({
-            readingTimeTarget: '.post-reading-time',
-            wordCountTarget: '.post-word-count',
-        });
-        
-        // Creates Captions from Alt tags
-        $(".post-content img").each(function() {
-            // Let's put a caption if there is one
-            if($(this).attr("alt"))
-              $(this).wrap('<figure class="image"></figure>')
-              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
-        });
-        
+    var $document = $(document);
+    var $body = $(document.body);
+    var $content = $('.content', $body);
+
+    $(".post-content").fitVids();
+
+    // Calculates Reading Time
+    $('.post-content').readingTime({
+      readingTimeTarget: '.post-reading-time',
+      wordCountTarget: '.post-word-count',
     });
+
+    // Creates Captions from Alt tags
+    $(".post-content img").each(function() {
+      // Let's put a caption if there is one
+      if($(this).attr("alt")) {
+        $(this).wrap('<figure class="image"></figure>')
+        .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+      }
+    });
+    // PJax bindings
+    // =================
+    if ($.support.pjax) {
+      $document.on('pjax:start', function() {
+        NProgress.start();
+        $body.scrollTop(0);
+      });
+
+      $document.on('pjax:end', function() {
+        if(typeof ga === 'function') {
+          ga('set', 'location', window.location.href);
+          ga('send', 'pageview');
+        }
+
+        if(typeof DISQUS === 'object' && $('#disqus_thread').length) {
+          DISQUS.reset({
+            reload: true,
+            config: function () {
+              this.page.identifier = disqus_identifier;
+            }
+          });
+        }
+
+        if(typeof DISQUSWIDGETS === 'object') {
+          DISQUSWIDGETS.getCount();
+        }
+
+        $('[data-load-image]', $content).each(function() {
+          //ImageLoader.load($(this));
+        });
+
+        NProgress.done();
+      });
+
+      var _pjaxOptions = {
+        container: '[data-pjax-container]',
+        fragment: '[data-pjax-container]'
+      };
+
+      $document.pjax('a[data-pjax]', _pjaxOptions);
+
+      $document.on('submit', 'form[data-pjax]', function(e) {
+        $.pjax.submit(e, _pjaxOptions);
+      });
+    }
+
+  });
 
 }(jQuery));
